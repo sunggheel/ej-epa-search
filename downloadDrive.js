bd98d6431655cab78fa63d97020918f35205b68a
Jerry Kang <jerry.kang@stonybrook.edu>
2023-06-25 19:05:58 -0400

    Finished drive downloading

--- downloadDrive.js
+++ downloadDrive.js
@@ -1,18 +1,25 @@
-const { GoogleAuth } = require('google-auth-library');
-const { google } = require('googleapis');
-
 const fs = require("fs");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
 async function downloadFromFolder(folderID) {
+    let pdfDocuments = fs.readdirSync("drive");
+    let pdfDocumentsSet = new Set(pdfDocuments);
+
     let response = await fetch(`https://www.googleapis.com/drive/v3/files?q='${folderID}'+in+parents&key=${process.env.GOOGLE_API_KEY}`);
     let data = await response.json();
 
     for (let file of data.files) {
-        if (file.mimeType === "application/pdf") downloadPDF(file.name, file.id);
-        return;
+        if (pdfDocumentsSet.has(file.name)) {
+            console.log(`document already downloaded: ${file.name}`);
+            continue;
+        }
+
+        if (file.kind !== "drive#file") continue;
+        if (file.mimeType !== "application/pdf") continue;
+        
+        downloadPDF(file.name, file.id);
     }
 }
 
@@ -20,11 +27,13 @@ async function downloadFromFolder(folderID) {
 
 async function downloadPDF(fileName, fileID) {
     let response = await fetch(`https://drive.google.com/uc?export=download&id=${fileID}`);
-    let blob = await response.blob();
+    let arrayBuffer = await response.arrayBuffer();
+
+    let buffer = Buffer.from(arrayBuffer)
     
-    fs.writeFileSync(`drive/${fileName}`, blob);
+    fs.writeFileSync(`drive/${fileName}`, buffer);
 
-
+    console.log(`successfully downloaded from drive: ${fileName}`);
 }
 
 let folderID = "1CnNzJbk61stAZYg_i2wuGtiKSq6FrWbx";
